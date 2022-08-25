7d30efee110da5f146bc11997d5a9a1deefd3a5a
VasuDevrani <vasu2002devrani@gmail.com>
2022-08-25 14:26:09 +0530

    changed a bit

--- src/features/films.tsx
+++ src/features/films.tsx
@@ -1,20 +1,20 @@
 import axios from "axios";
 import { ContextActions } from "../Store";
 
-let base = 'https://api.themoviedb.org/3'
+let base = 'https://api.themoviedb.org/3';
 
 const getPopularMovies = async (query: string = "", page: number = 1, id: string = "", category: string = "" ) => {
   let URL: string;
   if(query.length > 0)
   {
-    URL = `${base}/search/movie?api_key=9927d57067753126d627ab0540ed625a&query=${query}&page=${page}`;
+    URL = `${base}/search/movie?api_key=${API}&query=${query}&page=${page}`;
   }else if (id.length > 0){
-    URL = `${base}/discover/movie?api_key=9927d57067753126d627ab0540ed625a&with_genres=${id}&page=${page}`
+    URL = `${base}/discover/movie?api_key=${API}&with_genres=${id}&page=${page}`
   }else if(category.length > 0){
-    URL = `${base}/movie/${category}?api_key=9927d57067753126d627ab0540ed625a&language=en-US&page=${page}`
+    URL = `${base}/movie/${category}?api_key=${API}&language=en-US&page=${page}`
   }
   else{
-    URL = `${base}/movie/popular?api_key=9927d57067753126d627ab0540ed625a&language=en-US&page=${page}`;
+    URL = `${base}/movie/popular?api_key=${API}&language=en-US&page=${page}`;
   }
   const { data } = await axios.get(URL);
   return data;
@@ -22,28 +22,28 @@ const getPopularMovies = async (query: string = "", page: number = 1, id: string
 
 const getMovieDetails = async (id: string) => {
   const { data } = await axios.get(
-    `${base}/movie/${id}?api_key=9927d57067753126d627ab0540ed625a&append_to_response=videos`
+    `${base}/movie/${id}?api_key=${API}&append_to_response=videos`
   );
   return data;
 };
 
 const getCastData = async (id: string) => {
   const { data } = await axios.get(
-    `${base}/movie/${id}?api_key=9927d57067753126d627ab0540ed625a&append_to_response=credits`
+    `${base}/movie/${id}?api_key=${API}&append_to_response=credits`
   );
   return data;
 };
 
 const getPerson = async (id: string) => {
   const { data } = await axios.get(
-    `${base}/person/${id}?api_key=9927d57067753126d627ab0540ed625a&language=en-US&append_to_response=movies`
+    `${base}/person/${id}?api_key=${API}&language=en-US&append_to_response=movies`
   );
   return data;
 };
 
 const getPersonMovies = async(id: string) => {
   const { data } = await axios.get(
-    `${base}/discover/movie?with_cast=${id}&api_key=9927d57067753126d627ab0540ed625a`
+    `${base}/discover/movie?with_cast=${id}&api_key=${API}`
   );
   return data.results;
 }
@@ -69,7 +69,7 @@ const getMovies = async (
 
 export { getPopularMovies, getMovies, getMovieDetails, getCastData, getPerson, getPersonMovies };
 
-// ${base}/discover/movie?with_cast=2511949&api_key=9927d57067753126d627ab0540ed625a&language=en-US&sort_by=popularity.desc  cast movies
-// ${base}/person/56322?api_key=9927d57067753126d627ab0540ed625a&language=en-US  cast details
-// ${base}/movie/150540?api_key=9927d57067753126d627ab0540ed625a&append_to_response=credits   cast and credits
-// ${base}/movie/150540?api_key=9927d57067753126d627ab0540ed625a   movie details
+// ${base}/discover/movie?with_cast=2511949&api_key=${API}&language=en-US&sort_by=popularity.desc  cast movies
+// ${base}/person/56322?api_key=${API}&language=en-US  cast details
+// ${base}/movie/150540?api_key=${API}&append_to_response=credits   cast and credits
+// ${base}/movie/150540?api_key=${API}   movie details
